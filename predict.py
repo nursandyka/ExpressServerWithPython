c537774d1a26aeb45eef493bea87fbfd736cc771
aftadizca <aftadizca@gmail.com>
2020-07-20 00:11:15 +0700

    update

--- predict.py
+++ predict.py
@@ -4,27 +4,56 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 from dbn.tensorflow import SupervisedDBNClassification
+import cv2
+import face_recognition
+
+
+# def prediction(x_test):
+#     # Restore
+#     classifier = SupervisedDBNClassification.load('model.pkl')
+#     pred = classifier.predict(x_test)
+#     return pred
 
 
 x_test = []
 
-# Ubah input menjadi bytes
-im_bytes = base64.b64decode(str.encode(sys.argv[1]))
+# # Ubah input menjadi bytes
+# im_bytes = base64.b64decode(str.encode(sys.argv[1]))
+# # Ubah byte menjadi gambar
+# im_file = BytesIO(im_bytes)
+# # Ubah gambar menjadi PIL Object
+# img = Image.open(im_file)
+# # Convert ke Grayscale
+# img = img.convert('L')
+# # Convert ke Grayscale
+# data = np.asarray(img)
+# data = data.flatten()  # make 1 dimension
+# data = data/255  # image pixel normalization
+# x_test.append(data)
+
+
+img_bytes = base64.b64decode(str.encode(sys.argv[1]))
+im_arr = np.frombuffer(img_bytes, dtype=np.uint8)
 # Ubah byte menjadi gambar
-im_file = BytesIO(im_bytes)
-# Ubah gambar menjadi PIL Object
-img = Image.open(im_file)
-# Convert ke Grayscale
-img = img.convert('L')
-# Convert ke Grayscale
-data = np.asarray(img)
-data = data.flatten()  # make 1 dimension
-data = data/255  # image pixel normalization
-x_test.append(data)
+# img = BytesIO(img_bytes)
+# img = Image.open(img)
+img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# detect the (x,y)-coordinates of the bounding boxes
+# corresponding to each face in the input image
+# we are assuming the the boxes of faces are the SAME FACE or SAME PERSON
+boxes = face_recognition.face_locations(rgb_image, model='hog')
+# compute the facial embedding for the face
+# creates a vector of 128 numbers representing the face
+encodings = face_recognition.face_encodings(rgb_image, boxes)
+encodings = np.array(encodings)
+encodings = encodings.clip(0)
 
-
-classifier = SupervisedDBNClassification.load('model.pkl')
-pred = classifier.predict(x_test)
+if len(encodings > 0):
+    classifier = SupervisedDBNClassification.load('model.pkl')
+    pred = classifier.predict(encodings)
+else:
+    pred = ['bukandosen']
 
 
 # Hasil
