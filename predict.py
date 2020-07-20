4f803bcb3e87706704621777662a77957ee71df5
aftadizca <aftadizca@gmail.com>
2020-07-20 13:26:56 +0700

    update

--- predict.py
+++ predict.py
@@ -2,12 +2,17 @@ import sys
 import base64
 import numpy as np
 from dbn.tensorflow import SupervisedDBNClassification
+
 import cv2
 import face_recognition
 
+# decode base64 to bytes
 img_bytes = base64.b64decode(str.encode(sys.argv[1]))
+# convert img bytes to array
 im_arr = np.frombuffer(img_bytes, dtype=np.uint8)
+# convert array into image
 img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+# convert BGR format to RGB format
 rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # detect the (x,y)-coordinates of the bounding boxes
 # corresponding to each face in the input image
@@ -16,11 +21,15 @@ boxes = face_recognition.face_locations(rgb_image, model='hog')
 # compute the facial embedding for the face
 # creates a vector of 128 numbers representing the face
 encodings = face_recognition.face_encodings(rgb_image, boxes)
+# change into numpy array
 encodings = np.array(encodings)
+# replace value < 0 into 0 (replace negative value to zero)
 encodings = encodings.clip(0)
 
 if len(encodings > 0):
+    # load model
     classifier = SupervisedDBNClassification.load('model.pkl')
+    # klasifikasi
     pred = classifier.predict(encodings)
 else:
     pred = ['bukandosen']
